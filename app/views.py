924843db431ebe276fea2f97f7f387412f2a9903
dvssumanth <bobbysumanth4681@gmail.com>
2023-08-02 16:33:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dvs(request):
+    return render(request,'dvs.html')
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bike(request):
+    return render(request,'bike.html')
